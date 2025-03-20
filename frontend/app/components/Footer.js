33c00b9a862302b5cd4edb3242a9b30d2f1ab4ca
Kahl-d <email.khalidmkhan@gmail.com>
2025-03-19 19:03:49 -0700

    clean

--- frontend/app/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-white text-gray-600 text-center py-4 shadow-md">
-      <p>© 2025 Type. **Effortless Writing.**</p>
-    </footer>
-  );
-}
